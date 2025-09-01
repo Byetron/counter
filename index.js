a2730d4dc6f4b5ba932057136637e19a7b257424
Byetron <naval.adhya@gmail.com>
2025-09-01 18:07:47 +0530

    Experimenting

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById("H1").textContent = "This is Latin";
+document.getElementById("P1").textContent = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur, quas ex sequi ducimus aspernatur temporibus!";
+
+let x = 69;
+let name = "Claw"
+let gpa = 8.26;
+console.log(x);
+console.log(`My name is ${name}`);
+console.log(`My gpa is ${gpa}`);
